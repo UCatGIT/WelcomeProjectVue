49a29d957bcc8065ad764ffb61693773f084185d
MrTryAgain <mrtryagain2021@gmail.com>
2021-09-06 09:53:57 +0200

    commit for beginn Projekt

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ title }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      title: "Ich will Wochenende!!!"
+
+};
+}
 }
 </script>
 
